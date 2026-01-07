e87816637084bd72fb2c70452e2490cc837c18eb
Salvatore <pepesalvatore12@gmail.com>
2026-01-07 16:52:50 +0100

    add exe 3

--- src/es3/Exe3.java
+++ src/es3/Exe3.java
@@ -1,4 +1,27 @@
 package es3;
 
+import java.util.Scanner;
+
 public class Exe3 {
+    static void main(String[] args) {
+//        scrivi un programma che ciede di inserire una stringa e la suddivida in caratteri separati da virgola. il programma si
+//        interrompe quando l'utente inserisce la strinha ": q"
+        Scanner scanner = new Scanner(System.in);
+
+        String testo = scanner.nextLine();
+
+        while (true) {
+            System.out.println("inserisci del testo o digita `q` per uscire");
+            testo = scanner.nextLine();
+            if (testo.equals("q")) {
+                System.out.println("programma terminato");
+                break;
+            }
+//            googlando ho trovato che per separare i caratteri di una stringa da virgola si fa così
+            String testoFinale = String.join(",", testo.split(""));
+
+            System.out.println("la tua stringa ora è: " + testoFinale);
+        }
+        scanner.close();
+    }
 }
