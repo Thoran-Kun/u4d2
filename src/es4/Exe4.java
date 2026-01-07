95e3f24cdffd4d50eb9e7bd7af00b4d5357f15a3
Salvatore <pepesalvatore12@gmail.com>
2026-01-07 17:02:38 +0100

    add exe 4

--- src/es4/Exe4.java
+++ src/es4/Exe4.java
@@ -1,4 +1,16 @@
 package es4;
 
+import java.util.Scanner;
+
 public class Exe4 {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci un numero per far partire il conto alla rovescia: ");
+        int countDown = Integer.parseInt(scanner.nextLine());
+
+        for (int i = countDown; i >= 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("BOOOOM");
+    }
 }
