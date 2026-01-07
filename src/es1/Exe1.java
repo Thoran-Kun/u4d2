150233712daca061d27247bf640563d71db68006
Salvatore <pepesalvatore12@gmail.com>
2026-01-07 16:05:49 +0100

    add 1 exe

--- src/es1/Exe1.java
+++ src/es1/Exe1.java
@@ -1,4 +1,27 @@
 package es1;
 
 public class Exe1 {
+    static void main(String[] args) {
+//        accetta una stringa e ritorna true se il numero di caratteri è pari e false se dispari
+        String testo = "Questa è una stringa di testo Java";
+        int lunghezzaTesto = testo.length();
+
+        if (lunghezzaTesto % 2 == 0) {
+            System.out.println("La lunghezza è pari: true");
+        } else {
+            System.out.println("la lunghezza è dispari: false");
+        }
+
+        //------------------------------------- Exe2 ------------------------------------------
+
+//        accetta un anno espresso come intero e turna true se bisestile, false se non lo è.
+
+        int annoBisestile = 2000;
+        if ((annoBisestile % 4 == 0 && annoBisestile % 100 != 0) || (annoBisestile % 400 == 0)) {
+            System.out.println("L'anno " + annoBisestile + " digitato è Bisestile");
+        } else {
+            System.out.println("L'anno " + annoBisestile + " digitato NON è Bisestile");
+        }
+    }
+
 }
--- src/es2/Exe2.java
+++ src/es2/Exe2.java
@@ -1,4 +1,7 @@
 package es2;
 
 public class Exe2 {
+    static void main(String[] args) {
+        
+    }
 }
